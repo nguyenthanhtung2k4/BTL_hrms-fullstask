53af0cdec36adfeafae778ae6c3e728060aa67ce
JUSST <88431113+nguyenthanhtung2k4@users.noreply.github.com>
2026-06-15 15:04:28 +0700

    Cau hinh  Kien truc  chung

---
+++ shared/Hrms.Shared/Persistence/UpdateAuditableEntitiesInterceptor.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Hrms.Shared.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Hrms.Shared.Persistence;
+
+public class UpdateAuditableEntitiesInterceptor : SaveChangesInterceptor
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public UpdateAuditableEntitiesInterceptor(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        UpdateEntities(eventData.Context);
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        UpdateEntities(eventData.Context);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private void UpdateEntities(DbContext? context)
+    {
+        if (context == null) return;
+
+        var email = _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.Email) ?? "System";
+
+        foreach (var entry in context.ChangeTracker.Entries<IAuditableEntity>())
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTime.UtcNow;
+                entry.Entity.CreatedBy = email;
+            }
+
+            if (entry.State == EntityState.Added || entry.State == EntityState.Modified)
+            {
+                entry.Entity.LastModifiedAt = DateTime.UtcNow;
+                entry.Entity.LastModifiedBy = email;
+            }
+        }
+    }
+}
